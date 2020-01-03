e1239833f32bd2940ef009cad68ed821117486f0
Spyrex <malikfk100@gmail.com>
2020-01-03 17:59:26 +0100

    playbutton added

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -6,5 +6,20 @@ export var functionToDo : String
 
 func _ready():
 	$Label.text = btnText
+
+func _on_Button_pressed():
 	if nodePath != "" and functionToDo != "":
 		get_node(nodePath).call(functionToDo)
+	elif functionToDo != "":
+		callFunction(functionToDo)
+
+func callFunction(fnc):
+	var script = GDScript.new()
+	script.set_source_code("extends Node\n\nfunc run():\n\t" + fnc)
+	script.reload()
+	
+	var obj = Node.new()
+	obj.set_name("sciptCaller")
+	add_child(obj)
+	obj.set_script(script)
+	obj.run()
