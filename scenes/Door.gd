1e9b4da987b0da885bfdf2da2ad56730a3d226a0
Antricks <mdecker@posteo.de>
2020-01-04 10:52:16 +0100

    ich hoffe ich überschreibe deinen Kram nicht

--- scenes/Door.gd
+++ scenes/Door.gd
@@ -1,9 +1,15 @@
 extends StaticBody2D
 
-func open():
-	$AnimationPlayer.play("DoorOpen")
+export (String) var scene_to_change_to;
+export (Vector2) var position_to_get_to;
 
+var active = false;
+var playerBody;
+
+func open():
+	$AnimationPlayer.play("DoorOpen");
 
 func _on_Area2D_body_entered(body):
-	if body.name == "Player":
-		open()
+	if body.name == "Player" && body.has_method("transition"):
+		body.transition(position_to_get_to, scene_to_change_to);
+		open();
