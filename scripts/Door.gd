466980709a6a74bbe9e20f08fad14b3b8f18bf87
Antricks <mdecker@posteo.de>
2020-01-04 12:45:48 +0100

    door system updates

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,12 +1,15 @@
 extends StaticBody2D
 
-export (String) var scene_to_change_to;
-export (Vector2) var position_to_go_to;
+export (String) var scene_path;
+export (Vector2) var position_goal;
 
 func open():
 	$AnimationPlayer.play("DoorOpen")
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
-		body.transition(position_to_go_to, scene_to_change_to);
 		open()
+		$"/root/SceneChanger".change_scene(scene_path, 0);
+		yield($"/root/SceneChanger".animation_player, "animation_finished");
+		yield(get_tree().create_timer(0.2), "timeout");
+		$"/root/Global".player_node.position = position_goal;
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var playerNodePath
+var player_node
+
+func register_player(_player_node):
+	player_node = _player_node;
