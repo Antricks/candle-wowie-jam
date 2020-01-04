24916ac986d0a15981cfafff7d375dbdad576751
Antricks <mdecker@posteo.de>
2020-01-04 13:58:54 +0100

    added more title animation

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -4,6 +4,8 @@ export var btnText : String
 export var nodePath : String
 export var functionToDo : String
 
+signal play_pressed
+
 func _ready():
 	$Label.text = btnText
 
@@ -11,8 +13,9 @@ func _on_Button_pressed():
 	if nodePath != "" and functionToDo != "":
 		get_node(nodePath).call(functionToDo)
 	elif functionToDo != "":
-		callFunction(functionToDo)
-
+		callFunction(functionToDo)	
+	emit_signal("play_pressed");
+	
 func callFunction(fnc):
 	var script = GDScript.new()
 	script.set_source_code("extends Node\n\nfunc run():\n\t" + fnc)
--- scripts/StartMenu.gd
+++ scripts/StartMenu.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 func _ready():
 	$Cam/AnimationPlayer.play("Wackeln")
-	$Cam/GUI/AnimationPlayer2.play("titleIDLE")
+	$Cam/GUI/title/AnimationPlayer2.play("titleIDLE");
 	$Cam/GUI/AnimationPlayer.play("FadeIn")
 	$BG/AudioStreamPlayer2D.play(0)
+	
+
+func _on_PlayButton_pressed():
+	$Cam/GUI/AnimationPlayer2.play("titlePLAY");
+	yield($Cam/GUI/AnimationPlayer2, "animation_finished");
