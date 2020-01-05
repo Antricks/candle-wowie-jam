2bfe211edb7f1ea89f3334ccebe3cc74dc39a850
Antricks <mdecker@posteo.de>
2020-01-05 13:49:04 +0100

    added player lifes

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -4,6 +4,15 @@ var playerNodePath
 var player_node
 var player_position : Vector2; 
 var player_init_position : Vector2;
+var player_lifes : int;
+
+func get_player_lifes():
+	return player_lifes;
+
+func save_player_lifes(_lifes):
+	player_lifes = _lifes;
+
+######
 
 func get_saved_player_position():
 	return player_position;
@@ -11,11 +20,15 @@ func get_saved_player_position():
 func save_player_position(_position):
 	player_position = _position;
 
+######
+
 func get_saved_player_init_position():
 	return player_init_position;
 
 func save_player_init_position(_position):
 	player_init_position = _position;
+
+######
 
 func register_player(_player_node):
 	player_node = _player_node;
