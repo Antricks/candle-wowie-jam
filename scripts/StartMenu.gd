f5e6ea6af02c48393ebe447cebcf9da1d482fa49
Antricks <mdecker@posteo.de>
2020-01-05 17:29:53 +0100

    ui updates

--- scripts/StartMenu.gd
+++ scripts/StartMenu.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 func _ready():
 	$Cam/AnimationPlayer.play("Wackeln")
-	$Cam/GUI/title/AnimationPlayer2.play("titleIDLE");
+	$Cam/GUI/CenterContainer/title/AnimationPlayer2.play("titleIDLE");
 	$Cam/GUI/AnimationPlayer.play("fadeIn")
 	$BG/AudioStreamPlayer2D.play(0)
 
 func _on_PlayButton_pressed():
-	$Cam/GUI/title/AnimationPlayer2.play("titlePLAY");
-	yield($Cam/GUI/title/AnimationPlayer2, "animation_finished");
+	$Cam/GUI/CenterContainer/title/AnimationPlayer2.play("titlePLAY");
+	yield($Cam/GUI/CenterContainer/title/AnimationPlayer2, "animation_finished");
 	$Cam/GUI/AnimationPlayer.play("fadeOut");
 	yield($Cam/GUI/AnimationPlayer, "animation_finished");
 	$"/root/SceneChanger".change_scene("res://scenes/Main.tscn", 0);
