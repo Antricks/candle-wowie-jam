2c565b1dfd095ae6e9fbcc7a0ab8755070a62fd6
Antricks <mdecker@posteo.de>
2020-01-05 13:23:28 +0100

    cleanup of title animation, heilige scheiße war das unsauber gemacht

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -1,29 +1,26 @@
 extends TextureButton
 
 export var btnText : String
-export var nodePath : String
-export var functionToDo : String
-
-signal btn_pressed
 
 func _ready():
 	$Label.text = btnText
 
-func _on_Button_pressed():
-	if nodePath != "" and functionToDo != "":
-		get_node(nodePath).call(functionToDo)
-	elif functionToDo != "":
-		callFunction(functionToDo)	
-	emit_signal("btn_pressed");
-
-func callFunction(fnc, env = null):
-	var script = GDScript.new()
-	script.set_source_code("extends Node\n\nfunc run():\n\t" + fnc)
-	script.reload()
-	
-	var obj = Node.new()
-	obj.set_name("scriptCaller")
-	get_parent().add_child(obj)
-	obj.set_script(script)
-	obj.run()
-
+# DAS WAR MEGA UNNÖTIG, MAN KANN EINFACH DAS SIGNAL pressed()
+# IN DEM HAUPT SCRIPT VERWENDEN
+#
+#func _on_Button_pressed():
+#	if nodePath != "" and functionToDo != "":
+#		get_node(nodePath).call(functionToDo)
+#	elif functionToDo != "":
+#		callFunction(functionToDo)	
+#
+#func callFunction(fnc, env = null):
+#	var script = GDScript.new()
+#	script.set_source_code("extends Node\n\nfunc run():\n\t" + fnc)
+#	script.reload()
+#	
+#	var obj = Node.new()
+#	obj.set_name("scriptCaller")
+#	get_parent().add_child(obj)
+#	obj.set_script(script)
+#	obj.run()
--- scripts/StartMenu.gd
+++ scripts/StartMenu.gd
@@ -6,8 +6,9 @@ func _ready():
 	$Cam/GUI/AnimationPlayer.play("fadeIn")
 	$BG/AudioStreamPlayer2D.play(0)
 
-
 func _on_PlayButton_pressed():
 	$Cam/GUI/title/AnimationPlayer2.play("titlePLAY");
-	$Cam/GUI/AnimationPlayer.play("fadeOut");			#HELP Warum wird das nicht ausgeführt ?
 	yield($Cam/GUI/title/AnimationPlayer2, "animation_finished");
+	$Cam/GUI/AnimationPlayer.play("fadeOut");
+	$"/root/SceneChanger".change_scene("res://scenes/Main.tscn", 0);
+	yield($Cam/GUI/AnimationPlayer, "animation_finished");
